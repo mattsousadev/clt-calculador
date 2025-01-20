adc471b5e92d2db4303a5800b6e29bcda3ee088b
Matheus Sousa <matheus.sousa.dev@gmail.com>
2025-01-20 01:48:24 -0400

    feat: add 'salário líquido' screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,126 @@
+import { AppBar, Box, Container, FormControl, MenuItem, Select, TextField, Toolbar, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { CalculadorTrabalhista } from "./CalculadorTrabalhista";
 
+function formatBrl(value: number): string {
+  let brReais = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  });
+  return brReais.format(value)
+}
 
 function App() {
 
+  const calculador = new CalculadorTrabalhista(); 
+
+  const [calculationType, setCalculationType] = useState(1);
+  const [salarioLiquidoValue, setSalarioLiquidoValue] = useState(0.0);
+
+
+  const [salarioBruto, setSalarioBruto] = useState("");
+  const [dependentes, setDependentes] = useState("");
+
+  const [inss, setInss] = useState("1");
+  const [irrf, setIrrf] = useState("1");
+
+  const handleCalculationSelection = (e: any) => {
+    setCalculationType(e.target.value)
+  }
+
+  const handleSalarioBrutoChange = (e: any) => {
+    setSalarioBruto(e.target.value)
+  }
+
+  const handleDependentesChange = (e: any) => {
+    setDependentes(e.target.value)
+  }
+
+
+  useEffect(() => {
+    const salarioBrutoNumber = Number(salarioBruto)
+    const dependentesNumber = Number(dependentes)
+    if (!Number.isNaN(salarioBrutoNumber) && !Number.isNaN(dependentesNumber)){
+      const salarioLiquido = calculador.calcularSalarioLiquido(salarioBrutoNumber, dependentesNumber);
+      const inss = calculador.calcularInss(salarioBrutoNumber);
+      const irrf = calculador.calcularIrrf(salarioBrutoNumber-inss, dependentesNumber);
+      setSalarioLiquidoValue(salarioLiquido);
+      setInss(formatBrl(inss));
+      setIrrf(formatBrl(irrf));
+    }
+  }, [salarioBruto, dependentes])
+
   return (
-    <>
-      <div>
-        <h1>HELLO WORLD</h1>
-      </div>
-    </>
+    <Box sx={{ flexGrow: 1 }}>
+
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            CLT Calculador
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Container sx={{ maxWidth: "sm", mt: 2 }}>
+
+        <FormControl fullWidth>
+          <Select
+            id="demo-simple-select"
+            value={calculationType}
+            label="Age"
+            onChange={handleCalculationSelection}
+          >
+            <MenuItem value={1}>Salário Líquido</MenuItem>
+          </Select>
+        </FormControl>
+
+        <Typography sx={{ textAlign: "center", mt: 2 }} variant="h3" gutterBottom>
+          {formatBrl(salarioLiquidoValue)}
+        </Typography>
+
+        <Box
+          component="form"
+          sx={{ '& .MuiTextField-root': { m: 1, width: '100%' } }}
+          noValidate
+          autoComplete="off"
+        >
+          <div>
+            <TextField
+              type="number"
+              label="Salário Bruto"
+              placeholder="R$3000,00"
+              value={salarioBruto}
+              onChange={handleSalarioBrutoChange}
+            />
+            <TextField
+              type="number"
+              name="dependentes"
+              label="Nº Dependentes"
+              placeholder="2"
+              value={dependentes}
+              onChange={handleDependentesChange}
+            />
+            <TextField
+              disabled
+              name="inss"
+              label="INSS"
+              value={inss}
+              variant="filled"
+            />
+            <TextField
+              disabled
+              name="IRRF"
+              label="IRRF"
+              value={irrf}
+              variant="filled"
+            />
+          </div>
+        </Box>
+
+
+      </Container>
+
+    </Box>
   )
 }
 
